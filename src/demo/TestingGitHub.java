dd381e2abc0fede3e91ceb3e362fa453797b2d18
AshokRavi <ashokravi.pandranki@outlook.com>
2021-02-12 15:41:41 +0530

    changes are made in the project

--- src/demo/TestingGitHub.java
+++ src/demo/TestingGitHub.java
@@ -7,8 +7,14 @@ public class TestingGitHub {
 		
 		
 		
+		String name = "AshokRavi Pandranki";
+		String reverse = "";
 		
-		
+		for(int i =name.length()-1; i>=0;i--) {
+			reverse = reverse + name.charAt(i);
+			
+		}
+		System.out.println(reverse);
 		
 		
 		
