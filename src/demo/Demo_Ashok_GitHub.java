88ce49f019b3716e9853942b30d97a620cdbb65d
AshokRavi <ashokravi.pandranki@outlook.com>
2021-04-12 15:06:10 +0530

    This code contains swapping the Strings

---
+++ src/demo/Demo_Ashok_GitHub.java
@@ -0,0 +1,22 @@
+package demo;
+
+public class Demo_Ashok_GitHub {
+
+	public static void main(String[] args) {
+
+		String name1 = "Ashok";
+		String name2 = "Ravi";
+		
+		String temp= "";
+		
+		temp = name1;
+		name1 =name2;
+		name2 = temp;
+		
+		System.out.println(name1);
+		System.out.println(name2);
+		
+		
+	}
+
+}
